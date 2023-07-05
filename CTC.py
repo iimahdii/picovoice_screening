d87a2ebe96b5ce375ea8d75c21f84f528b79d74f
Mahdi Mirhosieni <106373112+iimahdii@users.noreply.github.com>
2023-07-05 13:33:04 -0400

    Create CTC.py

---
+++ CTC.py
@@ -0,0 +1,5 @@
+import keras
+from keras.layers import Dense, LSTM, TimeDistributed, Bidirectional, Input, Lambda
+from keras.optimizers import Adam
+from keras import backend as K
+from keras.models import Model
